b9f7d612a2e20e4f5fe3165f231b5f2c7e25b9f3
Uzair Ahmed <uzziahmed74@gmail.com>
2023-11-04 16:40:51 +0500

    4-NOV

--- src/Components/SIDEBAR/Sidebar.jsx
+++ src/Components/SIDEBAR/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Sidebar.css'
-import SidebarNavs from '../SidebarNavs/SIdebarNavs'
+import SidebarNavs from '../SidebarNavs/SidebarNavs'
 
 const Sidebar = () => {
   return (
